eb295053b792992736ca08cb03a905305a3886d5
sid-codes28 <268347456+sid-codes28@users.noreply.github.com.>
2026-09-08 16:53:57 +0530

    add more data types in variables.java

--- src/basics/Variables.java
+++ src/basics/Variables.java
@@ -3,14 +3,17 @@ package basics;
 public class Variables {
     public static void main(String[] args){
         String name = "Siddharth";
-        int age = 20;
+        int age = 20 , height = 177;
         double percentage = 85.5;
         char grade = 'A';
+        boolean isStudent = true;
 
         System.out.printf(
                 "Name       : %s%n" +
                 "Age        : %d%n" +
+                "Height     : %dcm%n" +
                 "Percentage : %.1f%n" +
-                "Grade      : %c%n",name,age,percentage,grade);
+                "Grade      : %c%n" +
+                "Student    : %b",name,age,height,percentage,grade,isStudent);
     }
 }
