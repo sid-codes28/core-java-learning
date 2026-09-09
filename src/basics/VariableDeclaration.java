a9d9a97869c5abf918cd26db65d065914c4dc792
sid-codes28 <268347456+sid-codes28@users.noreply.github.com.>
2026-09-10 00:57:04 +0530

    add variable declaration and initialization program

---
+++ src/basics/VariableDeclaration.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class VariableDeclaration {
+    public static void main(String[] args){
+        String name;
+        int num = 1;
+        System.out.println("Num initial value : "+num);
+        name = "Siddharth";
+        num = 5;
+
+        System.out.println("Name : "+name);
+        System.out.println("Num final value : "+num);
+
+    }
+}
