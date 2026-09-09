8bf0797d9d20c2480c6e363c3a5136015f7dbb07
sid-codes28 <268347456+sid-codes28@users.noreply.github.com.>
2026-09-09 17:11:21 +0530

    add Hello World java program

---
+++ src/basics/HelloWorld.java
@@ -0,0 +1,9 @@
+package basics;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Core Java!");
+        System.out.println("My Java learning journey starts here.");
+    }
+}
