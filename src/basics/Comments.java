a574d41260809652fb32582750b6ca47b3f932f3
sid-codes28 <268347456+sid-codes28@users.noreply.github.com.>
2026-09-09 17:37:24 +0530

    add commments program

---
+++ src/basics/Comments.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class Comments {
+    public static void main(String[] args){
+        //single line comment
+
+        /*
+        multi-line comment
+         */
+
+        /**
+         * javadoc/documentation comments
+         */
+    }
+}
